c0e88429d6c6363192fdd489d2f623166d51e449
Samuel Arno Saputra <samuelarnosaputra@gmail.com>
2017-11-06 16:29:38 +0700

    Try Ionicons

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 
 export default class App extends React.Component {
   render() {
@@ -8,6 +9,7 @@ export default class App extends React.Component {
         <Text>Opening up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
+        <Ionicons name='ios-alert-outline' color='teal' size={100} />
       </View>
     );
   }
