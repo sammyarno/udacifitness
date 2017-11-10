7e237578bddded10c758d17c61bf2ebc92667a9a
Samuel Arno Saputra <samuelarnosaputra@gmail.com>
2017-11-10 15:20:13 +0700

    Add slider stepper and icon inside the AddEntry

--- App.js
+++ App.js
@@ -1,25 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { View } from 'react-native';
+import AddEntry from './components/AddEntry';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Opening up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Ionicons name='ios-alert-outline' color='teal' size={100} />
+      <View>
+        <AddEntry />
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
