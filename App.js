b2a208aa95aa3de568342d52f8647decb83dee26
Samuel Arno Saputra <samuelarnosaputra@gmail.com>
2017-11-21 21:05:25 +0700

    History done

--- App.js
+++ App.js
@@ -6,13 +6,15 @@ import { Provider } from 'react-redux';
 import reducer from './reducers';
 
 import AddEntry from './components/AddEntry';
+import History from './components/History';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
-          <AddEntry />
+          <View style={{height: 70}} />
+          <History />
         </View>
       </Provider>
     );
