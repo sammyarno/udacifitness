ebde148a112a1b7ba25ab4d810439a31cb96b611
Samuel Arno Saputra <samuelarnosaputra@gmail.com>
2017-11-27 11:48:49 +0700

    Add undetermined status

--- components/Live.js
+++ components/Live.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
-import { View, Text, ActivityIndicator } from 'react-native';
+import { View, Text, ActivityIndicator, TouchableOpacity, StyleSheet } from 'react-native';
+import { Foundation } from '@expo/vector-icons';
+import { purple, white } from '../utils/colors';
 
 class Live extends Component {
 
   state = {
     coords: null,
-    status: null,
+    status: 'undetermined',
     direction: ''
   }
 
@@ -28,8 +30,16 @@ class Live extends Component {
 
     if(status === 'undetermined') {
       return (
-        <View>
-          <Text>Access Undetermined!</Text>
+        <View style={styles.center}>
+          <Foundation name='alert' size={50}/>
+          <Text>
+            You need to enable location services for this app.
+          </Text>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>
+              Enable
+            </Text>
+          </TouchableOpacity>
         </View>
       )
     }
@@ -41,5 +51,30 @@ class Live extends Component {
     )
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  center: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: purple,
+    alignSelf: 'center',
+    borderRadius: 5,
+    margin: 20,
+  },
+  buttonText: {
+    color: white,
+    fontSize: 20,
+  }
+})
 
 export default Live
